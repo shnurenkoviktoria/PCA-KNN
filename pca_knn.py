c377d1d57c5e86315c283af248d9af89181528b6
osm1nozhek <viktshnu-ipt27@lll.kpi.ua>
2023-11-27 12:06:36 +0200

    Initial commit

---
+++ pca_knn.py
@@ -0,0 +1,37 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+
+x_train = x_train.astype('float32') / 255.0
+x_val = x_val.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+x_train = x_train.reshape(x_train.shape[0], -1)
+x_val = x_val.reshape(x_val.shape[0], -1)
+x_test = x_test.reshape(x_test.shape[0], -1)
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_val = scaler.transform(x_val)
+x_test = scaler.transform(x_test)
+
+n = 100
+pca = PCA(n_components=n)
+x_train_pca = pca.fit_transform(x_train)
+x_val_pca = pca.transform(x_val)
+x_test_pca = pca.transform(x_test)
+
+knn_classifier = KNeighborsClassifier(n_neighbors=3)
+knn_classifier.fit(x_train_pca, y_train)
+
+y_pred = knn_classifier.predict(x_test_pca)
+accuracy = metrics.accuracy_score(y_test, y_pred)
+
+print(f'Мodel accuracy: {accuracy * 100:.2f}%')

5c7ff456b65e104101d90c1a4a0d310164fa5129
osm1nozhek <viktshnu-ipt27@lll.kpi.ua>
2023-11-27 12:10:34 +0200

    Initial commit

--- pca_knn.py
+++ pca_knn.py
@@ -7,11 +7,13 @@ from tensorflow.keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train, y_train, test_size=0.2, random_state=42
+)
 
-x_train = x_train.astype('float32') / 255.0
-x_val = x_val.astype('float32') / 255.0
-x_test = x_test.astype('float32') / 255.0
+x_train = x_train.astype("float32") / 255.0
+x_val = x_val.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
 
 x_train = x_train.reshape(x_train.shape[0], -1)
 x_val = x_val.reshape(x_val.shape[0], -1)
@@ -34,4 +36,4 @@ knn_classifier.fit(x_train_pca, y_train)
 y_pred = knn_classifier.predict(x_test_pca)
 accuracy = metrics.accuracy_score(y_test, y_pred)
 
-print(f'Мodel accuracy: {accuracy * 100:.2f}%')
+print(f"Мodel accuracy: {accuracy * 100:.2f}%")
